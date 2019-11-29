83b64b7bf3216d7020cee7306ead62be69063c98
Hisami Kurita <47776346+hisamikurita@users.noreply.github.com>
2019-11-29 12:31:31 +0900

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () =>{
+    const canvas01 = document.querySelector('.canvas_02');
+    const canvas02 = document.querySelector('.canvas_04');
+    canvas01.classList.add('active');
+    canvas02.classList.add('active');
+})
